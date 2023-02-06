8101d849d0996ef8a0584aaf31566b985555401b
John Lukach <84751182+jblukach@users.noreply.github.com>
2023-02-06 11:31:33 +0000

    2.63.2 (build e08e34a)

--- snap4n6/snap4n6_stack.py
+++ snap4n6/snap4n6_stack.py
@@ -34,7 +34,6 @@ class Snap4N6Stack(Stack):
             self, suppressions = [
                 {'id': 'AwsSolutions-VPC3','reason': 'GitHub Issue'},
                 {'id': 'AwsSolutions-S1','reason': 'GitHub Issue'},
-                {'id': 'AwsSolutions-S10','reason': 'GitHub Issue'},
                 {'id': 'AwsSolutions-IAM4','reason': 'GitHub Issue'},
                 {'id': 'AwsSolutions-IAM5','reason': 'GitHub Issue'},
                 {'id': 'AwsSolutions-SF2','reason': 'GitHub Issue'}
@@ -44,19 +43,9 @@ class Snap4N6Stack(Stack):
         account = Stack.of(self).account
         region = Stack.of(self).region
 
-        if region == 'ap-northeast-1' or region == 'ap-south-1' or region == 'ap-southeast-1' or \
-            region == 'ap-southeast-2' or region == 'eu-central-1' or region == 'eu-west-1' or \
-            region == 'eu-west-2' or region == 'me-central-1' or region == 'us-east-1' or \
-            region == 'us-east-2' or region == 'us-west-2': number = str(1)
-
-        if region == 'af-south-1' or region == 'ap-east-1' or region == 'ap-northeast-2' or \
-            region == 'ap-northeast-3' or region == 'ap-southeast-3' or region == 'ca-central-1' or \
-            region == 'eu-north-1' or region == 'eu-south-1' or region == 'eu-west-3' or \
-            region == 'me-south-1' or region == 'sa-east-1' or region == 'us-west-1': number = str(2)
-
         layer = _lambda.LayerVersion.from_layer_version_arn(
             self, 'layer',
-            layer_version_arn = 'arn:aws:lambda:'+region+':070176467818:layer:getpublicip:'+number
+            layer_version_arn = 'arn:aws:lambda:'+region+':070176467818:layer:getpublicip:3'
         )
 
 ### BUCKET ###
@@ -67,6 +56,7 @@ class Snap4N6Stack(Stack):
             block_public_access = _s3.BlockPublicAccess.BLOCK_ALL,
             removal_policy = RemovalPolicy.DESTROY,
             auto_delete_objects = True,
+            enforce_ssl = True,
             versioned = True
         )
 
